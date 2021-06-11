5fcbe5cc7a559f52d46b34c741dedc9775ec8298
Claire F <claire.fournier07@gmail.com>
2021-06-11 10:16:32 +0200

    correction package again

--- covid_project/preproc.py
+++ covid_project/preproc.py
@@ -1,9 +1,9 @@
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
 import tensorflow_io as tfio
-from tensorflow.keras import Sequential, layers, models, optimizers
-from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.layers.experimental.preprocessing import Rescaling
+#from tensorflow.keras import Sequential, layers, models, optimizers
+#from tensorflow.keras.callbacks import EarlyStopping
+#from tensorflow.keras.layers.experimental.preprocessing import Rescaling
 
 
 def train_val_test_split(X, y):
@@ -20,11 +20,10 @@ AUTOTUNE = tf.data.experimental.AUTOTUNE
 #TARGET_HEIGHT = 128  #à définir (hauteur de l'image redimensionnée)
 #TARGET_WIDTH = 128  #à définir (largeur de l'image redimensionnée)
 
-
 def extract_img(file_path,
                 label,
-                target_height=TARGET_HEIGHT,
-                target_width=TARGET_WIDTH):
+                target_height=128,
+                target_width=128):
     dcm_file = tf.io.read_file(file_path)
     img = tfio.image.decode_dicom_image(dcm_file)
     img = tf.image.resize_with_pad(img, target_height, target_width)
@@ -32,8 +31,10 @@ def extract_img(file_path,
     return img, label
 
 
-def prepare_ds(ds, buffer_size=BUFFER_SIZE, batch_size=BATCH_SIZE):
-    ds = ds.map(extract_img, num_parallel_calls=AUTOTUNE)
+def prepare_ds(ds, target_height=128,
+                target_width=128, buffer_size=32, batch_size=32):
+    ds = ds.map(extract_img(target_height, target_width),
+                num_parallel_calls=AUTOTUNE)
     ds = ds.cache()
     ds = ds.shuffle(buffer_size)
     ds = ds.batch(batch_size, num_parallel_calls=AUTOTUNE)
